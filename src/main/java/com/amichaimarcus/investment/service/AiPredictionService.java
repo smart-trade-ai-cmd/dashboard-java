4c260e0f08e4571bdfd68905b10242b0e08204f1
smart-trade-ai-cmd <smarttradeaiamichaimarcusinves@gmail.com>
2026-05-29 20:40:57 +0300

    Update AiPredictionService.java

--- src/main/java/com/amichaimarcus/investment/service/AiPredictionService.java
+++ src/main/java/com/amichaimarcus/investment/service/AiPredictionService.java
@@ -17,7 +17,7 @@ public class AiPredictionService {
     private PredictionHistoryRepository predictionHistoryRepository;
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String AI_SERVER_URL = "https://lstm-pyton-eu.onrender.com/";
+    private final String AI_SERVER_URL = "https://lstm-pyton-eu.onrender.com/api/ai/predict/";
 
     public AiPredictionDto getPredictionForSymbol(String symbol) {
         String url = AI_SERVER_URL + symbol;
